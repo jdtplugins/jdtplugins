68b709c3927340f535986352c492115ec878ecaa
norimania <norimania@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-06-10 16:05:51 +0000

    add neverclick

---
+++ jdtneverclick/js/jdtneverclick.js
@@ -0,0 +1,25 @@
+/**
+ * jDTneverClick
+ *
+ * @author        nori (http://twitter.com/5509)
+ * @copyright     (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
+ * @lastmodified  2010/06/09 21:03
+ */
+(function($) {
+
+	$.fn.jdtNeverClick = function(options) {
+		var c = $.extend({
+			listener: 'click dblclick contextmenu',
+			dialog: true,
+			message: 'ちょっ、まじクリックとかでありえないんですけど。'
+		}, options);
+		
+		$(this).bind(c.listener, function() {
+			if ( c.dialog ) {
+				alert( c.message );
+			}
+			return false;
+		});
+	}
+
+})(jQuery);
