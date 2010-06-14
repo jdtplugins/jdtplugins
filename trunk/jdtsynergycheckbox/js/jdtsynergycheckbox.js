796b5665c81fd9680c62fdb24abf9201dfc13244
norimania <norimania@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-06-14 05:36:44 +0000

    これは…ｗ

--- trunk/jdtsynergycheckbox/js/jdtsynergycheckbox.js
+++ trunk/jdtsynergycheckbox/js/jdtsynergycheckbox.js
@@ -14,20 +14,46 @@
 		var c = $.extend({
 				message: [
 					'俺も！俺も！'
-				]
+				],
+				special: false
 			}, options);
 			
 		$(this).click(function() {
 			if ( !$(this).is(':checkbox') ) return false;
 			var $name = $(this).attr('name');
-
-			if ( !$(this).is(':checked') ) {
-				$('input[name="'+ $name +'"]').removeAttr('checked');
-			} else {
-				$('input[name="'+ $name +'"]').attr('checked','checked');
-			}
 			
-			alert('チェックボックスたち: 「' + c.message[Math.floor(Math.random() * c.message.length)] + '」');
+			if ( !c.special ) {
+				if ( !$(this).is(':checked') ) {
+					$('input[name="'+ $name +'"]').removeAttr('checked');
+				} else {
+					$('input[name="'+ $name +'"]').attr('checked','checked');
+				}
+				
+				alert('チェックボックスたち: 「' + c.message[Math.floor(Math.random() * c.message.length)] + '」');
+			} else {
+				if ( !$(this).is(':checked') ) {
+					$(this).removeAttr('checked');
+				} else {
+					var sp = $('input[name="'+ $name +'"]').not(this);
+					for ( var i=0; i<sp.length; i++ ) {
+						if ( i==(sp.length-1) ) {
+							alert('じゃあ俺も!');
+							$(sp[i]).attr('checked','checked');
+							
+							alert('どうぞどうぞ');
+							$(this).removeAttr('checked');
+							for ( var j=0; j<sp.length; j++ ) {
+								if ( j!=(sp.length-1) ) {
+									$(sp[j]).removeAttr('checked');
+								}
+							}
+						} else {
+							alert('チェックボックス「俺も！」');
+							$(sp[i]).attr('checked','checked');
+						}	
+					}	
+				}	
+			}
 		});
 	}
 
