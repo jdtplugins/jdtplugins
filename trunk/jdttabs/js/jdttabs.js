29daf915853b3e7c5b64772bf924b334c92dc0e3
norimania <norimania@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-06-15 14:34:33 +0000

    lightbox

--- trunk/jdttabs/js/jdttabs.js
+++ trunk/jdttabs/js/jdttabs.js
@@ -1,7 +1,7 @@
 /**
  * jDTTabs
  *
- * @Version		 $Rev$
+ * @version      $Rev$
  * @author       nori (http://twitter.com/5509)
  * @copyright    (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
  * @license      The MIT License
