02474b254c887db9163feadf34153add5f4a0e91
norimania <norimania@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-06-17 11:58:55 +0000

    リンクまちがってた

--- jdtrunaway/js/jdtrunaway.js
+++ jdtrunaway/js/jdtrunaway.js
@@ -5,7 +5,7 @@
  * @author       nori (http://twitter.com/5509)
  * @copyright    (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
  * @license      The MIT License
- * @link         http://jdtplugins.googlecode.com/svn/trunk/jdtlightbox/index.html
+ * @link         http://jdtplugins.googlecode.com/svn/trunk/jdtrunaway/index.html
  *
  * $Date$
  */
