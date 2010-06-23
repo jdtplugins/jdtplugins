0d9b1e96d3b6dbe3aad4d456eb20092b6342d5e7
norimania <norimania@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-06-23 03:40:13 +0000

    タイトル間違えてた

--- jdtnoquery/js/jdtnoquery.js
+++ jdtnoquery/js/jdtnoquery.js
@@ -1,7 +1,6 @@
 /**
- * jDTTabs
+ * jDTNoQuery
  *
- * @version      $Rev$
  * @author       nori (http://twitter.com/5509)
  * @copyright    (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
  * @license      The MIT License
