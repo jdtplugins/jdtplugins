4bcfa582aca1e4e1fbe470c32600158975ea7147
labs.k5m <labs.k5m@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-08-10 07:31:29 +0000

    ｈｔｍｌ5対応にしてみたよ

--- jdtlinkdoubleclick/js/jdtlinkdoubleclick.js
+++ jdtlinkdoubleclick/js/jdtlinkdoubleclick.js
@@ -2,7 +2,7 @@
  * jDTLinkDoubleClick
  *
  * @version      $Rev$
- * @author       kamiyam
+ * @author       kamiyam (http://twitter.com/kamiyam)
  * @copyright    (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
  * @license      The MIT License
  * @link         http://jdtplugins.googlecode.com/svn/trunk/jdtlinkdoubleclick/index.html
@@ -14,16 +14,25 @@
 
 	$.fn.jdtLinkDouleClick = function()
 	{
-		$(this).each(function() 
+		
+		
+		this.each(function() 
 		{
+			//リンクキャンセル
 			$(this).attr("onClick","return false;");
+			
+			//リンク先取得
 			var href = $(this).attr("href");
 				
+			//ダブルクリックの動作設定
 			$(this).dblclick(function () 
 			{
 				document.location = href;
 			});
 		});
+		
+		//jQueryオブジェクトを返す
+		return ( this );
 
 	};
 				
