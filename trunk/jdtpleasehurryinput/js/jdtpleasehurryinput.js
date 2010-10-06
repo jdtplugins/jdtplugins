d5900940dba1ef968a668e6fb2fb76a6fcce2d67
labs.k5m <labs.k5m@68f5e56f-b2c4-8e11-07ca-6198aeb8c83e>
2010-10-06 15:28:50 +0000

    ネタナッシング。つまりは最軽量

---
+++ trunk/jdtpleasehurryinput/js/jdtpleasehurryinput.js
@@ -0,0 +1,40 @@
+/**
+ * jDTPleaseHurryInput
+ *
+ * @version      $Rev: 144 $
+ * @author       kamiyam (http://twitter.com/kamiyam)
+ * @copyright    (c) 2010 誰が使うの？何の役にも立たないjQueryプラグイン
+ * @license      The MIT License
+ * @link         http://jdtplugins.googlecode.com/svn/trunk/jdtpleasehurryinput/index.html
+ *
+ * $Date: 2010-07-29 18:37:19 +0900 $
+ */
+(function($) 
+{
+	jdtPHI = 
+	{};
+	
+    //5カウント
+    var limitCount = 5 * 1000;
+    
+    //10カウント実行
+    $.fn.jdtPleaseHurryInput = function() 
+    {
+ 		$(this).each( function() 
+		{
+			$(this).focus( function()
+				{
+					var that = this;
+					var obj = setTimeout( function ()
+									{
+										$(that).attr("disabled", "disabled");
+									},
+									limitCount  );
+					$(this).blur( function(){ clearTimeout( obj ); });
+				} );
+		});
+		
+		return this;
+    };
+    
+})(jQuery)
